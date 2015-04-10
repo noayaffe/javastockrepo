4ce24b1d9ddd6cdac8f52566fb03b955c8fa6ccf
noayaffe <yaffenoa@gmail.com>
2015-04-10 16:49:16 +0300

    exercise 2

--- src/com/noa/javacourse/NoaServlet.java
+++ src/com/noa/javacourse/NoaServlet.java
@@ -7,7 +7,14 @@ import javax.servlet.http.*;
 public class NoaServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
+		int num1 = 4;
+		int num2 = 3;
+		int num3 = 7;
+		int result = (num1 + num2) * num3;
+		String resultStr = new String("Result of ( " + num1 + " + " + num2 + " ) * " + num3 + " = " + result);
+		
+				
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println(resultStr);
 	}
 }
